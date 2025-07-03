81247b42597a074fb9c1617bdb890dcafc436ddb
VladDREEMURR <vladislavharcenko14@gmail.com>
2025-07-03 10:44:07 +0300

    Изменён главный layout

--- app/src/main/java/com/example/hideseekmapapp/MainActivity.kt
+++ app/src/main/java/com/example/hideseekmapapp/MainActivity.kt
@@ -1,29 +1,22 @@
 package com.example.hideseekmapapp
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hideseekmapapp.ui.theme.HideSeekMapAppTheme
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.mapview.MapView
 
+
+
 class MainActivity : ComponentActivity() {
-    private lateinit var mapView:MapView
+    private lateinit var mapView : MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("60b6e681-e142-4dd6-8f98-73996515ab97")
         MapKitFactory.initialize(this@MainActivity)
         setContentView(R.layout.activity_main)
-
         mapView = findViewById(R.id.map_view)
     }
 
@@ -37,21 +30,5 @@ class MainActivity : ComponentActivity() {
         super.onStop()
         MapKitFactory.getInstance().onStop()
         mapView.onStop()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HideSeekMapAppTheme {
-        Greeting("Android")
     }
 }
