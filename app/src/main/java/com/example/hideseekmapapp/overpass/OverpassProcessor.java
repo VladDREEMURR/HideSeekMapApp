7a7a10e1052e7316b970da901c0786f8981562c2
VladDREEMURR <vladislavharcenko14@gmail.com>
2025-10-14 16:02:43 +0300

    queryElementsWithGeometry работает ШИКАРНО! Всё, осталось приступить к сотавшимся вопросам

--- app/src/main/java/com/example/hideseekmapapp/overpass/OverpassProcessor.java
+++ app/src/main/java/com/example/hideseekmapapp/overpass/OverpassProcessor.java
@@ -3,6 +3,9 @@ package com.example.hideseekmapapp.overpass;
 import androidx.annotation.NonNull;
 
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateSequence;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 
@@ -13,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 
 import de.westnordost.osmapi.*;
-import de.westnordost.osmapi.common.*;
 import de.westnordost.osmapi.overpass.*;
 import de.westnordost.osmapi.map.data.*;
 import de.westnordost.osmapi.map.handler.*;
@@ -30,90 +32,31 @@ public class OverpassProcessor {
     // временное хранилище точек
     private ArrayList<Point> point_storage;
 
-
+    // для манипуляций геометрией
+    private GeometryFactory GF;
 
     private StringBuilder displayed_text = new StringBuilder();
-    private ArrayList<Point> points = new ArrayList<>();
-    private GeometryFactory GF = new GeometryFactory();
 
 
 
-    private final MapDataHandler mapdata_handler = new MapDataHandler() {
-        @Override
-        public void handle(BoundingBox bounds) {
-        }
-
-        @Override
-        public void handle(Node node) {
-            Point p = GF.createPoint(
-                    new Coordinate(node.getPosition().getLongitude(), node.getPosition().getLatitude())
-            );
-            points.add(p);
-        }
-
-        @Override
-        public void handle(Way way) {
-        }
-
-        @Override
-        public void handle(Relation relation) {
-            point_storage = new ArrayList<>();
-            List<RelationMember> rel_members = relation.getMembers();
-            for (RelationMember rm : rel_members) {
-//                overpass.queryElements();
-                overpass.queryElementsWithGeometry("", geom_handler);
-            }
-        }
-    };
 
 
-    // TODO: новый способ обработать геометрию, РАЗБЕРИСЬ
-    private final MapDataWithGeometryHandler geom_handler = new MapDataWithGeometryHandler() {
-        @Override
-        public void handle(@NonNull BoundingBox bounds) {
-
-        }
-
-        @Override
-        public void handle(@NonNull Node node) {
-
-        }
-
-        @Override
-        public void handle(@NonNull Way way, @NonNull BoundingBox bounds, @NonNull List<LatLon> geometry) {
-
-        }
-
-        @Override
-        public void handle(@NonNull Relation relation, @NonNull BoundingBox bounds, @NonNull Map<Long, LatLon> nodeGeometries, @NonNull Map<Long, List<LatLon>> wayGeometries) {
-
-        }
-    };
+    public OverpassProcessor() {
+        connection = new OsmConnection("https://maps.mail.ru/osm/tools/overpass/api/", "my user agent");
+        overpass = new OverpassMapDataApi(connection);
+        point_storage = new ArrayList<>();
+        GF = new GeometryFactory();
+    }
 
 
-    private final MapDataHandler way_rel_handler = new MapDataHandler() {
-        @Override
-        public void handle(BoundingBox bounds) {}
-
-        @Override
-        public void handle(Node node) {}
-
-        @Override
-        public void handle(Way way) {}
-
-        @Override
-        public void handle(Relation relation) {}
-    };
 
 
     public Point[] testOverpass() {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                connection = new OsmConnection("https://maps.mail.ru/osm/tools/overpass/api/", "my user agent");
-                overpass = new OverpassMapDataApi(connection);
                 try {
-                    overpass.queryElements(OverpassQueries.COMMERCIAL_AIRPORT, mapdata_handler);
+                    overpass.queryElementsWithGeometry(OverpassQueries.ADMINISTRATIVE_DISTRICT, geom_handler);
                 } catch (Exception e) {
                     StringWriter sw = new StringWriter();
                     PrintWriter pw = new PrintWriter(sw);
@@ -137,8 +80,70 @@ public class OverpassProcessor {
             displayed_text.append('\n');
         }
 
-        Point[] pts = new Point[points.size()];
-        points.toArray(pts);
+        Point[] pts = new Point[point_storage.size()];
+        point_storage.toArray(pts);
         return pts;
     }
+
+
+
+
+    private final MapDataWithGeometryHandler geom_handler = new MapDataWithGeometryHandler() {
+        @Override
+        public void handle(@NonNull BoundingBox bounds) {
+
+        }
+
+
+        @Override
+        public void handle(@NonNull Node node) {
+            Point p = GF.createPoint(
+                    new Coordinate(node.getPosition().getLongitude(), node.getPosition().getLatitude())
+            );
+            point_storage.add(p);
+        }
+
+
+        @Override
+        public void handle(@NonNull Way way, @NonNull BoundingBox bounds, @NonNull List<LatLon> geometry) {
+            ArrayList<Point> p_arr = new ArrayList<>();
+
+            for (LatLon ll : geometry) {
+                p_arr.add(GF.createPoint(
+                        new Coordinate(ll.getLongitude(), ll.getLatitude())
+                ));
+            }
+
+            Point[] pts = new Point[p_arr.size()];
+            p_arr.toArray(pts);
+            Geometry g = GF.createMultiPoint(pts);
+            point_storage.add(g.getCentroid());
+        }
+
+
+        @Override
+        public void handle(@NonNull Relation relation, @NonNull BoundingBox bounds, @NonNull Map<Long, LatLon> nodeGeometries, @NonNull Map<Long, List<LatLon>> wayGeometries) {
+            ArrayList<Point> p_arr = new ArrayList<>();
+
+            // nodes
+            for (LatLon ll : nodeGeometries.values()) {
+                p_arr.add(GF.createPoint(
+                        new Coordinate(ll.getLongitude(), ll.getLatitude())
+                ));
+            }
+            // ways
+            for (List<LatLon> ll_list : wayGeometries.values()) {
+                for (LatLon ll : ll_list) {
+                    p_arr.add(GF.createPoint(
+                            new Coordinate(ll.getLongitude(), ll.getLatitude())
+                    ));
+                }
+            }
+
+            Point[] pts = new Point[p_arr.size()];
+            p_arr.toArray(pts);
+            Geometry g = GF.createMultiPoint(pts);
+            point_storage.add(g.getCentroid());
+        }
+    };
 }
--- app/src/main/java/com/example/hideseekmapapp/overpass/OverpassQueries.java
+++ app/src/main/java/com/example/hideseekmapapp/overpass/OverpassQueries.java
@@ -4,30 +4,30 @@ public class OverpassQueries {
     public static String COMMERCIAL_AIRPORT =
             "[out:xml][timeout:25][bbox:55.489,37.216,55.989,38.206];\n" +
                     "nwr[\"aeroway\"=\"aerodrome\"][\"aerodrome:type\"=\"public\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String TRAIN_TERMINAL =
             "[out:xml][timeout:25][bbox:55.489,37.216,55.989,38.206];\n" +
                     "(\n" +
                     "  node[\"public_transport\"=\"station\"][\"railway\"=\"station\"][\"train\"=\"yes\"];\n" +
                     "  node[\"public_transport\"=\"station\"][\"railway\"=\"halt\"][\"train\"=\"yes\"];\n" +
                     ");\n" +
-                    "out meta;";
+                    "out geom;";
     public static String PARK =
             "[out:xml][timeout:25][bbox:55.489,37.216,55.989,38.000];\n" +
                     "nwr[\"leisure\"=\"park\"][\"name\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String THEME_PARK =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"tourism\"=\"theme_park\"][\"name\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String ZOO =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"tourism\"=\"zoo\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String GOLF_FIELD =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"leisure\"=\"golf_course\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String MUSEUM =
             "[out:xml][timeout:25][bbox:55.489,37.216,55.989,38.206];\n" +
                     "nwr[\"tourism\"=\"museum\"] -> .a;\n" +
@@ -36,29 +36,29 @@ public class OverpassQueries {
                     "  .a;\n" +
                     "  nwr.b[\"tourism\"=\"museum\"];\n" +
                     ") -> ._;\n" +
-                    "out meta;";
+                    "out geom;";
     public static String CINEMA =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"amenity\"=\"cinema\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String HOSPITAL =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"amenity\"=\"hospital\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String LIBRARY =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"amenity\"=\"library\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String FOREIGN_CONSULATE =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "nwr[\"diplomatic\"=\"embassy\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String DISTRICT =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "relation[\"type\"=\"boundary\"][\"boundary\"=\"administrative\"][\"admin_level\"=\"8\"];\n" +
-                    "out meta;";
+                    "out geom;";
     public static String ADMINISTRATIVE_DISTRICT =
             "[out:xml][timeout:25][bbox:55.550,37.216,55.989,38.000];\n" +
                     "relation[\"type\"=\"boundary\"][\"boundary\"=\"administrative\"][\"admin_level\"=\"5\"];\n" +
-                    "out meta;";
+                    "out geom;";
 }
